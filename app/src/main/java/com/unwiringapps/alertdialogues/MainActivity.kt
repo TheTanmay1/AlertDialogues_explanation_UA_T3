d5fa7b8e24fd48a8cb8c04e6a8b5e9b37e238d03
thetanmay1 <foundertanmay@gmail.com>
2022-09-10 06:25:01 +0530

    default dialog

--- app/src/main/java/com/unwiringapps/alertdialogues/MainActivity.kt
+++ app/src/main/java/com/unwiringapps/alertdialogues/MainActivity.kt
@@ -2,10 +2,39 @@ package com.unwiringapps.alertdialogues
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.unwiringapps.alertdialogues.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+             binding.defaultAlertbtn.setOnClickListener {
+
+                 val builder = AlertDialog.Builder(this)
+                 builder.setMessage("Enter a color")
+                 builder.setTitle("Background Color")
+
+
+                  builder.setPositiveButton("Save"){ dialog, _ ->
+              Toast.makeText(this, "Thanks for Saving",Toast.LENGTH_SHORT ).show()
+                  }
+
+                 builder.setNegativeButton("Cancel"){ dialog, _ ->
+                     Toast.makeText(this, "Ohh You Canceled me",Toast.LENGTH_SHORT ).show()
+                 }
+
+                 val dialog = builder.create()
+                 dialog.show()
+
+             }
+
+
     }
 }
